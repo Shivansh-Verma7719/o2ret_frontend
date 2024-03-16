05875a79a8ee1e5cf2ccc955548330a80eedc01f
Shivansh Verma <shivanshverma082004@gmail.com>
2024-03-16 20:49:26 +0530

    cosmetic changes

--- src/components/ui/sparkles.tsx
+++ src/components/ui/sparkles.tsx
@@ -436,7 +436,7 @@ export const SparklesCore = (props: ParticlesProps) => {
 
 export function Sparkles() {
     return (
-      <div className="h-[25rem] bg-black flex flex-col items-center justify-center overflow-hidden rounded-md">
+      <div className="h-[25rem] w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-md">
         <h1 className="md:text-7xl text-6xl lg:text-9xl font-bold text-center text-white relative z-20">
           o2ret
         </h1>
