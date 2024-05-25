b08c0d579b9c72733988852610f44f4bb4744434
Shivansh Verma <shivanshverma082004@gmail.com>
2024-05-25 22:41:43 +0530

    hotclick added

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,13 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './index.css';
+import Copyicon from '../../assets/icons/copy-svgrepo-com.svg';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import Tickicon from '../../assets/icons/tick-svgrepo-com.svg';
 
-function footer() {
+export default function Footer() {
+    const [copyStatus, setCopyStatus] = useState(false); // To indicate if the text was copied
+
+    const onCopyText = () => {
+        setCopyStatus(true);
+        setTimeout(() => setCopyStatus(false), 3000); // Reset status after 2 seconds
+    };
+    const Icon = () => {
+        if (copyStatus) {
+            return (
+                <img src={Tickicon} alt="copy" width="20" height="20" />
+            );
+            }
+        else {
+            return (
+                <img src={Copyicon} alt="copy" width="20" height="20" />
+            );
+        }
+    }
     return (
         <div className="footer">
             <h1 className='text-black font-Satoshi scale-75'>Connect with us!</h1>
-            <a href= "mailto:contact@o2ret.com" className='email'> Email: contact@o2ret.com </a>
+            <div className='emailcont'>
+
+                <CopyToClipboard text='contact@o2ret.com' onCopy={onCopyText}>      
+                <button className='flex flex-row email icon'>
+                    <p className='mx-2 text-o2ret-blue'>contact@o2ret.com</p> 
+                    <Icon />
+                </button>
+                </CopyToClipboard>
+            </div>
         </div>
     );
 }
-
-export default footer;
