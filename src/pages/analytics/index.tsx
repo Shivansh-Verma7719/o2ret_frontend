6d87ee85dd52cc32cade9b6d66f7d24456a58580
Shivansh Verma <shivanshverma082004@gmail.com>
2024-06-01 00:40:19 +0530

    navbar update

--- src/pages/analytics/index.tsx
+++ src/pages/analytics/index.tsx
@@ -6,14 +6,13 @@ import './index.css';
 // Follow a 95-5 rule for the map and the navbar use the map as the main component and the navbar as a sidebar
 // Implement the Navbar component in the components directory first according to the figma design
 
-const AnalyticsPage: React.FC = () => {
-    return (
-        <div className="analytics-page">
-            <Navbar />
-            {/* iframe link to the map below for reference if needed for design */}
-            {/* <iframe src="https://www.google.com/maps/d/u/0/embed?mid=1-X2hHZ7X_pai2ETudwiUxlK_CfLhT4s&ehbc=2E312F"></iframe> */}
-        </div>
-    );
-};
+const AnalyticsPage: React.FC = () => (
+    <div className="analytics-page">
+        <Navbar />
+        {/* <div className='map-cont'>        
+            <iframe src="https://www.google.com/maps/d/u/0/embed?mid=1-X2hHZ7X_pai2ETudwiUxlK_CfLhT4s&ehbc=2E312F"></iframe>
+        </div> */}
+    </div>
+);
 
 export default AnalyticsPage;
