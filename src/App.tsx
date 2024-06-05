42d00463cf954c4fc844af5ac01b747c6ce79bf8
poojs <poojasurendran40@gmail.com>
2024-06-05 22:13:57 +0530

    sign_up page added

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css';
 import Landing from './pages/landing/index';
 import AnalyticsPage from './pages/analytics/index';
 import Login from './components/sign_up/Login';
+import Register from './components/register_page/Register'
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 
 function App() {
@@ -9,7 +10,8 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing/>} />
-        <Route path="/login" element={<Login />} />        
+        <Route path="/login" element={<Login />} />   
+        <Route path="/register" element={<Register />} /> 
         {/* Remove when page is complete */}
         {/* <Route path="/analytics" element={<AnalyticsPage/>} /> */}
         <Route path="/analytics" element={<AnalyticsPage/>} />
