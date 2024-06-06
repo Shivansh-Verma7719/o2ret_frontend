cc54614cf7d46d9671ad207fd663efa92e44ad04
Shivansh Verma <shivanshverma082004@gmail.com>
2024-06-06 13:10:58 +0530

    map update

--- src/components/navbar_analytics/index.tsx
+++ src/components/navbar_analytics/index.tsx
@@ -20,7 +20,7 @@ interface BottomMenuItem {
 const topMenuItems: MenuItem[] = [
   { title: 'Main Map', mapNo: 0, icon: HomeSimpleDoor },
   { title: 'Sales Heatmap', mapNo: 1, icon: HomeSimpleDoor },
-  { title: 'Market Penetration Heatmap', mapNo: 2, icon: HomeSimpleDoor },
+  { title: 'MP Heatmap', mapNo: 2, icon: HomeSimpleDoor },
 ];
 
 const bottomMenuItems: BottomMenuItem[] = [
